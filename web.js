bd1cb9710c6e76ee19b29c3c01a9d4bc40a50659
Ubuntu <ubuntu@ip-172-31-3-36.us-west-2.compute.internal>
2013-07-24 10:12:32 +0000

    tryied to change web.js file

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+var buffer =new Buffer("I'm a Fluffer","utf-8");
+var buffer2=new Buffer(console log (fs.readFile(index.html)),"utf-8");
+fs.readFile(index.html);
 app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
