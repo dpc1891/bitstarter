dee3315150a2571d62fb2928c1b47e977a1bf1ac
Ubuntu <ubuntu@ip-172-31-3-36.us-west-2.compute.internal>
2013-07-24 11:00:32 +0000

    trying readfile

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
-var buffer =new Buffer("I'm a Fluffer","utf-8");
-console.log(buffer);
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var readfile = fs.readfileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send("THis is really not working is it");
+  response.writeHead(200,{
+'Content-type':'text/html; charset=utf-8'
 });
-console.log("im tyring to out ut this also");
+response.end(readfile);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
